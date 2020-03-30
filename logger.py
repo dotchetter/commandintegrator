fa6ce452b967f87e693cf66a72efdcc13995fdeb
dotchetter <dotchetter@protonmail.ch>
2020-03-30 04:01:39 +0200

    Switched to capitalized letters in path.

--- logger.py
+++ logger.py
@@ -14,7 +14,7 @@ Module details:
 """
 
 
-with open ('commandintegrator.settings.json', 'r', encoding = 'utf-8') as f:
+with open (Path('CommandIntegrator') / 'commandintegrator.settings.json', 'r', encoding = 'utf-8') as f:
     LOG_DIR = Path(json.loads(f.read())['log_dir'])
 
 LOG_FILE = 'runtime.log'
