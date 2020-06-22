862da00cc9cf1c44f90e56c7f4a7d5dbe5c194ca
dotchetter <dotchetter@protonmail.ch>
2020-06-22 22:06:33 +0200

    Converted class to static, since having multiple instances of this object is illogical and not necessary.
    The same configuration for identifying pronouns should be the same
    across all Features using CI.

--- pronounlookuptable.py
+++ pronounlookuptable.py
@@ -2,43 +2,40 @@ from .enumerators import CommandPronoun
 
 class PronounLookupTable:
     """
-    Provide a grammatic framework that 
+    Provide a grammatic lookup that 
     returns a tuple of matches for certain
     grammatic classes of words found in a given
     sentence. 
     """
 
-    def __init__(self):
-        self._lookup_table = {
-            CommandPronoun.INTERROGATIVE: (
-                'vad', 'vem',
-                'hur', 'varför',
-                'vilken', 'vilket',
-                'hurdan', 'hurudan',
-                'undrar', 'när'),
+    _LOOKUP_TABLE = {
+        CommandPronoun.INTERROGATIVE: (
+            'vad', 'vem',
+            'hur', 'varför',
+            'vilken', 'vilket',
+            'hurdan', 'hurudan',
+            'undrar', 'när'),
 
-            CommandPronoun.PERSONAL: (
-                'jag', 'vi',
-                'du', 'ni',
-                'han', 'hon',
-                'den', 'de',
-                'dem'),
+        CommandPronoun.PERSONAL: (
+            'jag', 'vi',
+            'du', 'ni',
+            'han', 'hon',
+            'den', 'de',
+            'dem'),
 
-            CommandPronoun.POSSESSIVE: (
-                'mitt', 'mina',
-                'min', 'vårt',
-                'vår', 'våra',
-                'vårt', 'din',
-                'ditt', 'dina',
-                'ert', 'er',
-                'era', 'sin',
-                'sitt', 'sina')
-        }
+        CommandPronoun.POSSESSIVE: (
+            'mitt', 'mina',
+            'min', 'vårt',
+            'vår', 'våra',
+            'vårt', 'din',
+            'ditt', 'dina',
+            'ert', 'er',
+            'era', 'sin',
+            'sitt', 'sina')
+    }
 
-    def __repr__(self):
-        return f'PronounLookupTable({self._lookup_table})'
-
-    def lookup(self, message: list) -> tuple:
+    @staticmethod
+    def lookup(message: list) -> tuple:
         """
         Split a given string by space if present, to iterate
         over a sentence of words. Returns a tuple with enum
@@ -50,8 +47,8 @@ class PronounLookupTable:
         pronouns = []
 
         for word in message:
-            for key in self._lookup_table:
-                if word in self._lookup_table[key]:
+            for key in PronounLookupTable._LOOKUP_TABLE:
+                if word in PronounLookupTable._LOOKUP_TABLE[key]:
                     pronouns.append(key)
             if '?' in word:
                 pronouns.append(CommandPronoun.INTERROGATIVE)
