728bfe641fe2145c3c250d0cd1d3b1c6ff957102
dotchetter <dotchetter@protonmail.ch>
2020-07-21 12:26:31 +0200

    the __getitem__ method is written to return None if the callback does not match the given command, otherwise return the function associated with it (self._func)
    The callback instance will evaluate whether it contains any of the
    words provided in the 'lead' property first.
    If it does, and 'trail' property is left undefined, callback returns
    the function.
    
    If the 'trail' property is defined however - the message must also
    comply with any of the words in the trail parameter (string or collection
    of strings) - and all of these must occur _after_ the 'lead' strings
    to comply.
    
    If the 'ordered' property is set to True, in addition to the rules above,
    the message must _also_ comply with having every word in both
    lead and trail, if trail is defined, in the same order of appearance as
    in 'lead' and(if) _trail_.
    This is offloaded to a separate method, _assert_ordered.

--- core/callback.py
+++ core/callback.py
@@ -31,33 +31,70 @@ class Callback:
 	trail:
 		(tuple) words in sequence, that must be present 
 		after words in the _lead tuple
-	callable:
+	func:
 		method / function / callable that will execute 
 		if binding matches command
 	ordered:
-		(bool) whether or not the order of items in the 
-		lead or trail property is trivial
+		(bool) whether the order of items in the 
+		lead or trail property is trivial or not 
 	"""
 
-	__slots__ = ('_lead', '_trail', '_callable', '_bindings', '_ordered')
+	__slots__ = ('_lead', '_trail', '_func', '_bindings', 
+				 '_ordered', '_intact_lead', '_intact_trail')
 
-	def __init__(self, **kwargs):
-		self._trail = dict()
-		self._callable
-		self._bindings = dict()
-		self._ordered = False
-		
-		for key, value in kwargs:
-			setattr(self, key, value)
+	def __init__(self, lead, func, trail = None, ordered = False):
+		self.bindings = dict()
+		self.func = func
+		self.lead = lead
+		self.trail = trail
+		self.ordered = ordered
 
 	def __repr__(self):
 		pass
 
-	def __getitem__(self, word: Message) -> callable
-		pass
+	def __getitem__(self, message: Message) -> callable:
+		"""
+		Return the callable bound to the Callback instance
+		if the message matches the subset(s) of strings
+		defined in this object.
 
-	def __setitem__(self):
-		pass
+		To begin with, the message has to match at least
+		one word in the self.lead property. This is asserted
+		through the .intersection method, as to get the word(s)
+		that matches the self.lead property in a subset.
+		Next, the optional self.trail property is investigated
+		similarly if it is defined - otherwise not. 
+
+		The self.trail string / collection of strings has to,
+		by definition, appear after the words in self.lead.
+		This is asserted by first identifying the words that
+		matches the trail in the message. The words that also
+		are present in the lead are removed by subtraction.
+		Next, by iterating over the two collections the order
+		of appearence can now be determined by identifying
+		the index of the word compared between the two. If 
+		the index is higher in the trail than the lead, the
+		loop continues and will eventually exhaust. 
+		If not, the trail condition is not met and method 
+		exits with False.
+		:param message:
+			CommandIntegrator.Message
+		:returns:
+			callable or None
+		"""
+		match_trail = False
+
+		if not (match_lead := [i for i in self._lead if i in message.content]):
+			return None
+		elif self._ordered and not self._assert_ordered(message):
+			return None
+
+		if self._trail:
+			latest_lead_occurence, latest_trail_occurence = 0, 0
+
+			if not (match_trail := [i for i in self._trail if i in message.content]):
+				return None
+
 			for lead, trail in zip_longest(match_lead, match_trail):
 				try:
 					_index = message.content.index(lead)
@@ -77,14 +114,40 @@ class Callback:
 			return self._bindings[match_lead.pop()]
 		return None
 
+	def _assert_ordered(self, message: Message) -> bool:
+		ordered_trail = True
+		ordered_lead = True
+
+		for word_a, word_b in zip([i for i in message.content if i in self._lead], self._lead):
+			if word_a != word_b: 
+				ordered_lead = False
+				break
+
+		if not self._trail:	return ordered_lead
+
+		for word_a, word_b in zip([i for i in message.content if i in self._trail], self._trail):
+			if word_a != word_b: 
+				ordered_trail = False
+				break
+
+		return ordered_trail and ordered_lead
+
+	@property
+	def bindings(self) -> dict:
+		return self._bindings
+
+	@bindings.setter
+	def bindings(self, bindings: dict):
+		self._bindings = bindings
+
 	@property
 	def lead(self) -> tuple:
 		return self._lead
 	
 	@lead.setter
-	def lead(self, lead: tuple)
-		for i in lead:
-			self._bindings[lead] = self.callable
+	def lead(self, lead: tuple):
+		if isinstance(lead, str): lead = (lead,)		
+		for i in lead: self._bindings[i] = self._func
 		self._lead = lead
 
 	@property
@@ -93,13 +156,30 @@ class Callback:
 
 	@trail.setter
 	def trail(self, trail: tuple):
+		if trail is None: 
+			self._trail = None
+			return
+		if isinstance(trail, str): trail = (trail,)
+		if (collision := [i for i in self._lead if i in trail]):
+			raise AttributeError(f"trail illogical - reoccuring string from lead: '{collision.pop()}'")
+		
+		for i in trail:	self._bindings[i] = self._func
 		self._trail = trail
 
 	@property
-	def callable(self) -> callable:
-		return self._callable
+	def func(self) -> callable:
+		return self._func
 	
-	@callable.setter
-	def callable(self):
-		self._callable = callable
-	
+	@func.setter
+	def func(self, func: callable):
+		if not callable(func):
+			raise AttributeError(f"{func} cannot be used as func parameter as it is not callable")
+		self._func = func
+
+	@property
+	def ordered(self) -> bool:
+		return self._ordered
+
+	@ordered.setter
+	def ordered(self, ordered: bool):
+		self._ordered = ordered
