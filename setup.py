8ac17ebaae6d00ff1fa3e48801776ffcf7cf5975
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-12 14:27:34 +0200

    Added install requirements for pypi Added package definitions for pypi

--- setup.py
+++ setup.py
@@ -12,11 +12,32 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/commandintegrator",
-    packages=setuptools.find_packages(),
+    packages=[
+        'commandintegrator.baseclasses',
+        'commandintegrator.core',
+        'commandintegrator.models',
+        'commandintegrator.tools'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "aiohttp >= 3.6.2",
+        "async-timeout >= 3.0.1",
+        "attrs >= 19.3.0",
+        "certifi >= 2020.4.5.1",
+        "chardet >= 3.0.4",
+        "idna >= 2.9",
+        "multidict >= 4.7.5",
+        "pytz >= 2019.3",
+        "requests >= 2.23.0",
+        "style >= 1.1.0",
+        "update >= 0.0.1",
+        "urllib3 >= 1.25.8",
+        "websockets >= 8.1",
+        "yarl >= 1.4.2"
     ],
     python_requires='>=3.8',
 )
