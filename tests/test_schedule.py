729d238eeef1f5c94a67234534d46acd05ede1f7
Simon Olofsson <dotchetter@protonmail.ch>
2021-04-18 22:45:06 +0200

    test file for the schedule prototype.

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,33 +1,54 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest import TestCase, skip
-from commandintegrator.core.decorators import schedule
+import commandintegrator as ci
+from functools import partial
 
 
 async def recieve(msg=None):
-    print("The Foo.recieve got:", msg)
+    print("Async receiver:", msg)
+
+
+class TimeFeature:
+
+    def __init__(self, name):
+        self.name = name
+        # super().__init__()
+        # self.command_parser = ci.CommandParser(keywords="klockan")
+        # self.command_parser.callbacks = ci.Callback("klockan", self.get_time)
+        # ci.schedule.run(self.get_time, )
+
+    @ci.schedule.run(every="second", kwargs={"country": "sverige", "greeting": "hej"})
+    def get_time(self, country=None, greeting=None):
+        return f"{greeting}! Klockan är {datetime.now().strftime('%H:%M')} i {country}, {self}"
+
+    @ci.schedule.run(every="second", recipient=recieve)
+    def no_args(self):
+        return f"I just return this string as async: {self}"
 
 
 if __name__ == "__main__":
 
-    @schedule.method(every="second", kwargs={"what": "wohooo!"})
-    async def say(what):
-        raise AttributeError("This is constructed")
-        return f"{datetime.now().strftime('%H:%M:%S')}: {what}"
+    tf = TimeFeature(name="hejsan")
 
-    # @schedule.method(after="00:00:05")
+   #@ci.schedule.run(after="00:00:05", every="second")
     def two_seconds():
-        return "Two seconds!"
+        return "five seconds!"
 
-    #@schedule.method(every="second")
-    async def every_second():
-        return datetime.now().strftime("%H:%M:%S")
+    #@ci.schedule.run(every="second")
+    async def hello_world():
+        return "hello world"
 
-    # @schedule.method(every="friday", at="09:00")
+    # ci.schedule.schedule(func=hello_world, every="minute")
+
+    now = datetime.now()
+    now += timedelta(seconds=5)
+
+    @ci.schedule.run(exactly_at=now)
     def thankgoditsfriday():
-        return "Thank god its friday!"
-
+        return f"I ran {datetime.now()}"
     while 1:
-        time.sleep(1)
-        if job := schedule.outputs.get():
-            print("\r", job, end="")
+        time.sleep(0.01)
+        job = ci.schedule.outputs.get()
+        if job:
+            print(job.result)
